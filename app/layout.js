8b57667c1b57c3e7e2aa8ec49fb9957d8651ee00
Munya-Marinda <moyomarinda@gmail.com>
2024-05-23 22:17:24 +0200

    Integrated Vercel Analytics tracking traffic.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
